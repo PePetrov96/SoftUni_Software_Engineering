1434d937af1a1211c7d474ad92124584fa88e999
Peter Petrov <112096824+PePetrov96@users.noreply.github.com>
2022-09-15 23:44:46 +0300

    Update Task_2_Passed.java

--- 1_Programming_Fundaments/Homework/1_Basic Syntax, Conditional Statements and Loops/1_Lab/Task_2_Passed.java
+++ 1_Programming_Fundaments/Homework/1_Basic Syntax, Conditional Statements and Loops/1_Lab/Task_2_Passed.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Task_2_Passed {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        double grade = Double.parseDouble(scan.nextLine());
-        if (grade >= 3.00) {
+        if (scan.nextDouble() >= 3.00) {
             System.out.println("Passed!");
         }
     }
