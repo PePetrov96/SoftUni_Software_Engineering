266f6c6012c4264e714c3aa374f7738a86f7ae3a
PePetrov96 <sstrigger96@gmail.com>
2023-02-21 16:12:13 +0200

    update

--- 3_Java_OOP/Homework/5_Polymorphism/2_Exercise/Task_4_Word/CommandInterface.java
+++
@@ -1,6 +0,0 @@
-package Task_4_Word;
-
-public interface CommandInterface {
-    void init();
-    void handleInput(String input);
-}
--- 3_Java_OOP/Homework/5_Polymorphism/2_Exercise/Task_4_Word/TextTransform.java
+++
@@ -1,5 +0,0 @@
-package Task_4_Word;
-
-public interface TextTransform {
-    void invokeOn(StringBuilder text, int startIndex, int endIndex);
-}
