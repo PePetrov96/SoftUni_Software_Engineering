b75011c4a1bdae9766d40000c46640bb771e7e59
PePetrov96 <sstrigger96@gmail.com>
2023-03-02 20:19:05 +0200

    update

--- 3_Java_OOP/Homework/5_Polymorphism/2_Exercise/Task_3_Animal_Farm/Meat.java
+++
@@ -1,7 +0,0 @@
-package Task_3_Wild_Farm;
-
-public class Meat extends Food{
-    public Meat(int quantity) {
-        super(quantity);
-    }
-}
--- 3_Java_OOP/Homework/5_Polymorphism/2_Exercise/Task_3_Animal_Farm/Vegetable.java
+++
@@ -1,7 +0,0 @@
-package Task_3_Wild_Farm;
-
-public class Vegetable extends Food{
-    public Vegetable(int quantity) {
-        super(quantity);
-    }
-}
