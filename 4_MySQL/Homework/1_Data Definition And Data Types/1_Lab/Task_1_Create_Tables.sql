98f042f48c693a1015ab76696832db550cfe36db
PePetrov96 <sstrigger96@gmail.com>
2023-03-13 21:17:36 +0200

    add

---
+++ 4_MySQL/Homework/1_Data Definition And Data Types/1_Lab/Task_1_Create_Tables.sql
@@ -0,0 +1,18 @@
+USE softuni;
+
+CREATE TABLE `employees` (
+  `id` INT AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `categories` (
+  `id` INT AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `products` (
+  `id` INT AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `category_iD` INT NOT NULL,
+  PRIMARY KEY (`id`));
