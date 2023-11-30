3e7fe0d4edf56695071f4a0ff180c06e763e6e78
PePetrov96 <sstrigger96@gmail.com>
2023-11-30 14:33:13 +0200

    addd

--- 9_JS Front-End/Homework/7_HTTP and REST/1_Lab/02. GitHub-Repos/scripts/app.js
+++ 9_JS Front-End/Homework/7_HTTP and REST/1_Lab/02. GitHub-Repos/scripts/app.js
@@ -1,3 +1,10 @@
 function loadRepos() {
-   console.log("TODO...");
+    fetch('https://api.github.com/users/testnakov/repos')
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById('res').textContent = data;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
