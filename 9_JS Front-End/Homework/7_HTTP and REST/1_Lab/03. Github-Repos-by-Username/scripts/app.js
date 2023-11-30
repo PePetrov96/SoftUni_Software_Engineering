a6ea43fe5736de9a21a90871259fa838fbb94f7d
PePetrov96 <sstrigger96@gmail.com>
2023-11-30 20:12:37 +0200

    add

--- 9_JS Front-End/Homework/7_HTTP and REST/1_Lab/03. Github-Repos-by-Username/scripts/app.js
+++ 9_JS Front-End/Homework/7_HTTP and REST/1_Lab/03. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,36 @@
 function loadRepos() {
-	console.log("TODO...");
+	// Get the username as input
+	let username = document.getElementById('username').value;
+
+	// Get the repo of that user. Make sure to return it as a JSON with .json()
+	fetch(`https://api.github.com/users/${username}/repos`)
+		.then(res => res.json())
+		.then(data => {
+			listRepos(data);
+		})
+		.catch(error => {
+			listRepos(error);
+		});
+
+	function listRepos(data) {
+		// Get the list
+		let list = document.getElementById('repos');
+
+		// Remove the current element (clear the list)
+		list.innerHTML = '';
+
+		// For each repo, append the name and link as an <a> element inside a <li> element
+		for (const entry of data) {
+			let li = document.createElement('li');
+			let a = document.createElement('a');
+
+			// Make use of the non-changeable JSON format GitHub provides
+			a.href = entry.url;
+			a.text = entry.full_name;
+
+			li.appendChild(a);
+
+			list.appendChild(li);
+		}
+	}
 }
--- 9_JS Front-End/Homework/7_HTTP and REST/1_Lab/04. Github-Commits/app.js
+++ 9_JS Front-End/Homework/7_HTTP and REST/1_Lab/04. Github-Commits/app.js
@@ -1,4 +1,31 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    // Get the username and repo from the input fields
+    let username = document.getElementById('username').value;
+    let repo = document.getElementById('repo').value;
+
+    // Get output list and clear previous content
+    let commitsList = document.getElementById('commits');
+    commitsList.innerHTML  = '';
+
+    // Fetch data from GitHub API
+    fetch(`https://api.github.com/repos/${username}/${repo}/commits`)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`Error: ${res.status} (Not Found)`);
+            }
+            return res.json();
+        })
+        .then(commits => {
+            // Process each commit and add it to the list
+            for (const commit of commits) {
+                let li = document.createElement('li');
+                li.textContent = `${commit.commit.author.name}: ${commit.commit.message}`;
+                commitsList.appendChild(li);
+            }
+        })
+        .catch(error => {
+            let li = document.createElement('li');
+            li.textContent = error.message;
+            commitsList.appendChild(li);
+        })
 }
