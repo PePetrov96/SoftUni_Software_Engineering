09acdbaf935cc69e57022f37ac78b56d43877cd3
PePetrov96 <sstrigger96@gmail.com>
2024-02-11 16:49:43 +0200

    update

--- 8_HTML & CSS/Exams/HTML & CSS Exam - 22 Octover 2023/Task_3_Business/script.js
+++ 8_HTML & CSS/Exams/HTML & CSS Exam - 22 Octover 2023/Task_3_Business/script.js
@@ -6,3 +6,21 @@ window.onscroll = function() {
         header.classList.remove('updated-class');
     }
 }
+
+function calculateFibonacci(number) {
+    if (number <= 1) {
+        return number;
+    }
+
+    let fibonacciPrevious = 0;
+    let fibonacciCurrent = 1;
+
+    // Calculate Fibonacci sequence up to the nth position
+    for (let i = 2; i <= number; i++) {
+        const temporary = fibonacciCurrent;
+        fibonacciCurrent = fibonacciPrevious + fibonacciCurrent;
+        fibonacciPrevious = temporary;
+    }
+
+    return fibonacciCurrent;
+}
